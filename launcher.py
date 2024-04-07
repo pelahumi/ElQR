068e5b14deef1523a070a1b632a5e1b66805baa5
Pelayo <phuermij@myuax.com>
2024-04-07 11:36:52 +0200

    Listo

--- launcher.py
+++ launcher.py
@@ -1,4 +1,20 @@
 from ElQR import Qr
 
 def launcher():
-    
+    path = 'img/flag.png'
+
+    qr = Qr(path)
+
+    flag_encoded = qr.read_qr()
+
+    print(f'Flag encoded: {flag_encoded}')
+
+    print('Decoding flag...')
+
+    while True:
+        try:
+            flag_encoded = qr.decode(flag_encoded)
+            print(flag_encoded, '\n')
+        except:
+            print('Mensaje descifrado:', flag_encoded)
+            break
